92877676f574f3460a784d07fb2fd1fa98457325
HLKellermann <helena.kellermann@acad.ufsm.br>
2026-04-27 21:07:48 -0300

    :bulb: docs: added comments on functions

--- e1-MatDiagonal/matDiagonal.c
+++ e1-MatDiagonal/matDiagonal.c
@@ -3,10 +3,13 @@
 #include <stdbool.h>
 #include "matDiagonal.h"
 
+//FUNCOES AUXILIARES
+
 /*---valida se a posicao digitada pelo usuario esta dentro do tamanho da matriz---*/
 bool posicao_valida(int lin, int col, int ordem){
     return (lin>=0 && lin<ordem) && (col>=0 && col<ordem);
 }
+
 
 /*---imprime o resultado da consulta feita na funcao 'consulta_elem'---*/
 void imprime_consulta(Diagonal* m, int lin, int col){
@@ -21,6 +24,7 @@ void imprime_consulta(Diagonal* m, int lin, int col){
 }
 
 
+/*---loop para tratar possivel entrada que nao seja um valor inteiro, para a posicao a se consultar em 'consulta_elem'---*/
 void posicoes_inteiras(int* linha, int* coluna){
     int invalido = true;
     int auxLin, auxCol;
@@ -37,61 +41,67 @@ void posicoes_inteiras(int* linha, int* coluna){
 }
 
 
+/*---loop para tratar possivel entrada que nao seja um valor inteiro(como char ou strings)---*/
 int input_eh_inteiro(){
     int input, aux;
     do{
         aux = scanf("%d", &input);  //passa entrada do usuario para variavel auxiliar
     
-        if(aux != 1){
+        if(aux != 1){   //se o scanf nao leu um int
             printf("Entrada invalida!Tente de novo: ");
             while(getchar() != '\n');       //limpa buffer
         }
     }while(aux != 1);
-    return input;
+    return input;   //retorna input ok
 }
 
+
+/*---loop ateh o input do usuario ser valido para o seu caso especifico---*/
 void valida_valores(int* input, int caso){
-    bool invalido;
+    bool invalido;  //loop ate o input ser valido(false)
     do{
-        if(caso == 1 && *input <= 0){
+        if(caso == 1 && *input <= 0){               //caso da funcao 'cria_mat()' para a var 'm.ordem'
             printf("Ordem deve ser maior que 0: ");
             invalido = true;
-        }else if(caso == 2 && (*input != 0 && *input != 1)){
+        }else if(caso == 2 && (*input != 0 && *input != 1)){    //caso da funcao 'consulta_elem()', saber se continua consultando
             printf("Digite 0 ou 1!: ");
             invalido = true;
         }else{
-            invalido = false;
+            invalido = false;   //se o input for valido, quebra loop e o valor do * de input estarah ok 
             break;
         }
-        *input = input_eh_inteiro();
+        *input = input_eh_inteiro();    //rele valor do input e confirma que eh int
     }while(invalido);
 }
 
 
+/*---função chamada pelas principais para realizar leitura e validação da entrada---*/
 int leitura_input(int caso){
-    int input;
+    int input;  
     switch (caso){
-    case 1:
-        printf("Digite a ordem da Matriz Diagonal: ");
-        input = input_eh_inteiro();
-        valida_valores(&input, 1);
-        return input;
+    case 1:         //se 'caso' for '1', funcao 'leitura_input' foi chamda pela principal 'cria_mat()'
+        printf("Digite a ordem da Matriz Diagonal: ");  
+        input = input_eh_inteiro(); //loop ate ler um input que seja tipo int, devolve quando ok
+        valida_valores(&input, 1);  //loop se o input for =< 0, rele ate ser um int e >0, "atualiza" 'input' quando ok 
+        return input;   //retorna para cria_mat(), pra var 'm.ordem'
     case 2:
         printf("\n[1] Repetir operacao   [0] Parar\t");
-        input = input_eh_inteiro();
-        valida_valores(&input, 2);
-        return input;
+        input = input_eh_inteiro(); //loop ate ler um input que seja tipo int, devolve quando ok
+        valida_valores(&input, 2);  //loop se o input for != de 1 e 0, rele ate ser um int e == a 1 ou 0, "atualiza" 'input' quando ok 
+        return input;   //retorna para 'consulta_elem()', para o 'while()'
     default:
-        printf("\nHa algo de errado no codigo!");
+        printf("\nHa algo de errado no codigo!");   //aparece apenas se alguem alterou o valor fixo de 'caso' em alguma das f principais
         exit(1);
     }
 }
+
+//FUNCOES PRINCIPAIS
 
 /*---criação da matriz---*/
 Diagonal cria_mat(){
     Diagonal m;
 //alocacao dinamica do vetor da matriz
-    m.ordem = leitura_input(1);
+    m.ordem = leitura_input(1);         //chama funcao que le e valida a entrada do usuario, retorna valor para m.ordem
     m.v = malloc(m.ordem * sizeof(int));
 //mensagem de erro
     if(m.v == NULL){
@@ -101,6 +111,7 @@ Diagonal cria_mat(){
     return m;   //retorna Diagnonal m para a Diagonal matriz no main 
 }
 
+
 /*---preenchimento da matriz---*/
 void preenche_mat(Diagonal* m){
     int i;
@@ -108,9 +119,10 @@ void preenche_mat(Diagonal* m){
 //laço para guardar apenas os valores da diagonal no vetor da matriz
     for(i=0; i<m->ordem; i++){
         printf("(%d,%d):", i, i);   //mostra a posicao que o valor serah guardado
-        m->v[i] = input_eh_inteiro();   //guarda valor informado no vetor
+        m->v[i] = input_eh_inteiro();   //guarda valor retornado no vetor
     }
 }
+
 
 /*---impressão da matriz---*/
 void imprime_mat(Diagonal m){
@@ -137,6 +149,7 @@ void consulta_elem(Diagonal* m){
     }while(leitura_input(2) == 1);
 }
 
+
 /*---libera vetor da matriz---*/
 void libera_mat(Diagonal *m){
     if(m!=NULL && m->v!=NULL){
@@ -145,6 +158,7 @@ void libera_mat(Diagonal *m){
     }
     m->ordem = 0;
 }
+
 
 //FUNCAO PRINCIPAL MAIN
 int main(int argc, char const *argv[])
